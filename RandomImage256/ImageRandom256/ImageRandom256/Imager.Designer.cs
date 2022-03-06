a427ed6f1dab823edd6fb875353eefb813c43967
dorian fonseca <ElCocoDeBongo.CryptoNFT@gmail.com>
2022-03-06 11:30:06 +0100

    RandomImageProject

---
+++ RandomImage256/ImageRandom256/ImageRandom256/Imager.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace ImageRandom256
+{
+    partial class Imager
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(810, 690);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1200, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw all color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1200, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Draw light color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1200, 432);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1266, 493);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1266, 461);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1200, 493);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "File Name :";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1200, 528);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "↺";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1200, 464);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Path :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1226, 406);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1080,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(1317, 406);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1080,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown2.TabIndex = 17;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1200, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "X :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1291, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Y :";
+            // 
+            // Imager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1548, 714);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Imager";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RandomImage256/ImageRandom256/ImageRandom256/Imager.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageRandom256
+{
+    public partial class Imager : Form
+    {
+        public Bitmap canvasBitmap { get; set; }
+        Graphics canvasGraphics;
+        int canvasWidth = 1;
+        int canvasHeight = 1;
+        int[,] canvasDotArray;
+        SolidBrush[] allColorArr = new SolidBrush[18] { new SolidBrush(Color.LightCoral), new SolidBrush(Color.Red), new SolidBrush(Color.LightGoldenrodYellow), new SolidBrush(Color.Yellow), new SolidBrush(Color.LightGreen), new SolidBrush(Color.Lime), new SolidBrush(Color.LightCyan), new SolidBrush(Color.Cyan), new SolidBrush(Color.LightBlue), new SolidBrush(Color.Blue), new SolidBrush(Color.LightPink), new SolidBrush(Color.Magenta), new SolidBrush(Color.DarkCyan), new SolidBrush(Color.Green), new SolidBrush(Color.DarkKhaki), new SolidBrush(Color.DarkMagenta), new SolidBrush(Color.Brown), new SolidBrush(Color.MediumBlue)};
+        SolidBrush[] LightColorArr = new SolidBrush[12] { new SolidBrush(Color.LightCoral), new SolidBrush(Color.Red), new SolidBrush(Color.LightGoldenrodYellow), new SolidBrush(Color.Yellow), new SolidBrush(Color.LightGreen), new SolidBrush(Color.Lime), new SolidBrush(Color.LightCyan), new SolidBrush(Color.Cyan), new SolidBrush(Color.LightBlue), new SolidBrush(Color.Blue), new SolidBrush(Color.LightPink), new SolidBrush(Color.Magenta)};
+        Random rnd = new Random();
+        List<int[]> rollBack;
+        int[] lastColors;
+        int color;
+
+        public Imager()
+        {
+            rollBack = new List<int[]>();
+            InitializeComponent();
+            InitilizeLastColors();
+            loadCanvas();
+            this.pictureBox1.MouseClick += new MouseEventHandler(this.pictureBox1_Click);
+        }
+        private void loadCanvas()
+        {
+            pictureBox1.Width = canvasWidth * (1080 / canvasWidth);
+            pictureBox1.Height = canvasHeight * (1080 / canvasHeight);
+            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            canvasGraphics.FillRectangle(Brushes.LightGray, 0, 0, canvasBitmap.Width, canvasBitmap.Height);
+            pictureBox1.Image = canvasBitmap;
+            canvasDotArray = new int[canvasWidth, canvasHeight];
+        }
+        private void LoopDraw(Brush[] colorArr)
+        {
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            for (int i = 0; i < canvasWidth; i++)
+            {
+                for (int j = 0; j < canvasHeight; j++)
+                {
+                    Draw(i, j, colorArr);
+                }
+            }
+            pictureBox1.Image = canvasBitmap;
+        }
+        private void Draw(int i, int j, Brush[] colorArr)
+        {
+            if (j != 0)
+            {
+                while (color == lastColors[j - 1] || color == lastColors[j])
+                {
+                    color = rnd.Next(colorArr.Count());
+                }
+            }
+            else
+            {
+                while (color == lastColors[j])
+                {
+                    color = rnd.Next(colorArr.Count());
+                }
+            }
+            canvasGraphics.FillRectangle(colorArr[color], j * (1080 / canvasWidth), i * (1080 / canvasWidth), (1080 / canvasWidth), (1080 / canvasWidth));
+            lastColors[j] = color;
+        }
+        private void pictureBox1_Click(object sender, MouseEventArgs e)
+        {
+            float x, y;
+            if (e.Button == MouseButtons.Right)
+            {
+                x = (e.X) / (900 / canvasWidth);
+                x = (float)Math.Floor(x);
+                y = (e.Y) / (900 / canvasWidth);
+                y = (float)Math.Floor(y);
+                if (canvasBitmap.GetPixel((int)(x) * (1080 / canvasWidth), (int)(y) * (1080 / canvasWidth)).Name == "ff000000")
+                {
+                    return;
+                }
+                rollBack.Add(new int[3] { (int)x, (int)y, FindColorInArr(canvasBitmap.GetPixel((int)(x) * (1080 / canvasWidth), (int)(y) * (1080 / canvasWidth)))});
+                ChangeColor(x, y, Brushes.Black);
+            }
+        }
+        private int FindColorInArr(Color canvasColor)
+        {
+            int a;
+            for(a = 0; a < allColorArr.Count(); a++)
+            {
+                if (allColorArr[a].Color.ToArgb() == canvasColor.ToArgb())
+                {
+                    return a;
+                }
+            }
+            return a;
+        }
+        private void InitilizeLastColors()
+        {
+            lastColors = new int[canvasHeight];
+            for(int p = 0; p < canvasHeight; p++)
+            {
+                lastColors[p] = 0;
+            }
+        }
+        private void ChangeColor(float x,float y, Brush color)
+        {
+            canvasGraphics = Graphics.FromImage(canvasBitmap);
+            canvasGraphics.FillRectangle(color, x * (1080 / canvasWidth), y * (1080 / canvasWidth), (1080 / canvasWidth), (1080 / canvasWidth));
+            pictureBox1.Image = canvasBitmap;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            color = 0;
+            rollBack = new List<int[]>();
+            canvasWidth = ((int)numericUpDown1.Value);
+            canvasHeight = ((int)numericUpDown2.Value);
+            InitilizeLastColors();
+            loadCanvas();
+            LoopDraw(allColorArr);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            color = 0;
+            rollBack = new List<int[]>();
+            canvasWidth = ((int)numericUpDown1.Value);
+            canvasHeight = ((int)numericUpDown2.Value);
+            InitilizeLastColors();
+            loadCanvas();
+            LoopDraw(LightColorArr);
+        }
+        public void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save(textBox2.Text+ @"\"+textBox1.Text+".png",System.Drawing.Imaging.ImageFormat.Png);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (rollBack.Count > 0)
+            {
+                int[] data = rollBack[rollBack.Count - 1];
+                ChangeColor(data[0], data[1], allColorArr[data[2]]);
+                rollBack.RemoveAt(rollBack.Count - 1);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
